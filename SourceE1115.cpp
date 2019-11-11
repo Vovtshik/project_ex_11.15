93fef902f31ce77c0e390f961312f8ede80c0660
Volodymir Konovalov <vvkon@meta.ua>
2019-11-11 09:14:52 +0200

    Update SourceE1115.cpp

--- SourceE1115.cpp
+++ SourceE1115.cpp
@@ -1,11 +1,21 @@
 #include "../std_lib_facilities.h"
 
-void in_file_text(string& name_file, vector<double>&vs); 
+void in_file_text(string& name_file, vector<double>&vn); 
 
 int main()
 {
+    vector<double>vn;
 
     return 0;
 }
 
-void in_file_text(string& name_file, vector<double>&vs)
+void in_file_text(string& name_file, vector<double>&vn)
+{
+    ifstream ist(name_file);
+    if(!ist) error("Unable to open input file ", name_file);
+    ist.exceptions(ist.exceptions() | ios_base::badbit);
+    for(double temp_num; ist >> temp_num;)
+    {
+        vn.push_back(temp_num);
+    }
+}
