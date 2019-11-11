cdf4866c67b6fac6eb1f30852634199152ee6cbe
Volodymir Konovalov <vvkon@meta.ua>
2019-11-11 10:03:20 +0200

    Update SourceE1115.cpp

--- SourceE1115.cpp
+++ SourceE1115.cpp
@@ -1,6 +1,7 @@
 #include "../std_lib_facilities.h"
 
-void in_file_text(string& name_file, vector<double>&vn); 
+void input_file(string& name_file, vector<double>&vn); 
+void formatted_output_file(vector<double>&vn);
 
 int main()
 {
@@ -9,7 +10,7 @@ int main()
     return 0;
 }
 
-void in_file_text(string& name_file, vector<double>&vn)
+void input_file(string& name_file, vector<double>&vn)
 {
     ifstream ist(name_file);
     if(!ist) error("Unable to open input file ", name_file);
@@ -18,4 +19,9 @@ void in_file_text(string& name_file, vector<double>&vn)
     {
         vn.push_back(temp_num);
     }
+}
+
+void formatted_output_file(vector<double>&vn)
+{
+    
 }
