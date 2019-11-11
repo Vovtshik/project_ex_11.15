64b0ce9087eda190bf8abe6e35505ee7efa8dcfb
Volodymir Konovalov <vvkon@meta.ua>
2019-11-11 09:06:15 +0200

    Update SourceE1115.cpp

--- SourceE1115.cpp
+++ SourceE1115.cpp
@@ -1,7 +1,11 @@
 #include "../std_lib_facilities.h"
 
+void in_file_text(string& name_file, vector<double>&vs); 
+
 int main()
 {
 
     return 0;
 }
+
+void in_file_text(string& name_file, vector<double>&vs)
